12d41c4757e102d3abf0079c96c82562fce7c101
Denis Carriere <carriere.denis@gmail.com>
2022-06-05 16:20:47 -0400

    move external contracts

--- contracts/eosio.yield/scripts/build.sh
+++ contracts/eosio.yield/scripts/build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # compile
-eosio-cpp eosio.yield.cpp -I include -I ../
+blanc++ eosio.yield.cpp -I ../ -I ../../external
 
-# external contracts
-if [ ! -f "./include/eosio.token/eosio.token.wasm" ]; then
-    eosio-cpp ./include/eosio.token/eosio.token.cpp -I include -o include/eosio.token/eosio.token.wasm
-fi
+# # external contracts
+# if [ ! -f "./include/eosio.token/eosio.token.wasm" ]; then
+#     eosio-cpp ./include/eosio.token/eosio.token.cpp -I include -o include/eosio.token/eosio.token.wasm
+# fi
 
-if [ ! -f "./include/eosio.system/eosio.system.wasm" ]; then
-    eosio-cpp ./include/eosio.system/eosio.system.cpp -I include -o include/eosio.system/eosio.system.wasm
-fi
+# if [ ! -f "./include/eosio.system/eosio.system.wasm" ]; then
+#     eosio-cpp ./include/eosio.system/eosio.system.cpp -I include -o include/eosio.system/eosio.system.wasm
+# fi
 
 # contracts
 CONTRACT="d.e.yield"
--- contracts/oracle.yield/scripts/build.sh
+++ contracts/oracle.yield/scripts/build.sh
@@ -4,20 +4,20 @@
 CONTRACT="d.o.yield"
 
 # compile
-eosio-cpp oracle.yield.cpp -I include -I ../
+blanc++ oracle.yield.cpp -I ../ -I ../../external
 
-# external contracts
-if [ ! -f "./include/eosio.token/eosio.token.wasm" ]; then
-    eosio-cpp ./include/eosio.token/eosio.token.cpp -I include -o include/eosio.token/eosio.token.wasm
-fi
+# # external contracts
+# if [ ! -f "./include/eosio.token/eosio.token.wasm" ]; then
+#     eosio-cpp ./include/eosio.token/eosio.token.cpp -I include -o include/eosio.token/eosio.token.wasm
+# fi
 
-if [ ! -f "./include/oracle.defi/oracle.defi.wasm" ]; then
-    eosio-cpp ./include/oracle.defi/oracle.defi.cpp -I include -o include/oracle.defi/oracle.defi.wasm
-fi
+# if [ ! -f "./include/oracle.defi/oracle.defi.wasm" ]; then
+#     eosio-cpp ./include/oracle.defi/oracle.defi.cpp -I include -o include/oracle.defi/oracle.defi.wasm
+# fi
 
-if [ ! -f "./include/delphioracle/delphioracle.wasm" ]; then
-    eosio-cpp ./include/delphioracle/delphioracle.cpp -I include -o include/delphioracle/delphioracle.wasm
-fi
+# if [ ! -f "./include/delphioracle/delphioracle.wasm" ]; then
+#     eosio-cpp ./include/delphioracle/delphioracle.cpp -I include -o include/delphioracle/delphioracle.wasm
+# fi
 
 # unlock wallet & deploy
 cleos wallet unlock --password $(cat ~/eosio-wallet/.pass)
