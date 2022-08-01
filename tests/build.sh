0d6c21217d8f4eeeb1e7484030d28c4668008f2e
Denis Carriere <carriere.denis@gmail.com>
2022-08-01 14:47:20 -0400

    add checksums to builds

--- tests/build.sh
+++ tests/build.sh
@@ -4,11 +4,14 @@
 echo "compiling... [oracle.yield]"
 cd contracts/oracle.yield
 blanc++ oracle.yield.cpp -I ../ -I ../../external
+shasum -a 256 oracle.yield.wasm
 
 echo "compiling... [eosio.yield]"
 cd ../eosio.yield
 blanc++ eosio.yield.cpp -I ../ -I ../../external
+shasum -a 256 eosio.yield.wasm
 
 echo "compiling... [admin.yield]"
 cd ../admin.yield
 blanc++ admin.yield.cpp -I ../ -I ../../external
+shasum -a 256 admin.yield.wasm
